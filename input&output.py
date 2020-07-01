bd96c8fcab5be203d1c2849a2910e94be86ddf0a
lazistar <hsjin1456@gmail.com>
2020-07-01 14:25:20 +0900

    입출력_quiz
    실생활에서 해볼만한 작업을 퀴즈로 풀어보았다.
    1~50주차 보고서 작성 퀴즈였다.

--- input&output.py
+++ input&output.py
@@ -10,8 +10,8 @@
 
 # 은행 대기순번표
 # 001, 002, 003, ...
-# for num in range(1,21):
-#     print("대기번호 : " + str(num).zfill(3))
+for num in range(1,21):
+    print("대기번호 : " + str(num).zfill(3))
 
 # answer = input("아무 값이나 입력하세요 : ")
 answer = 10
