b3aa2363f177001b90524060b2d055792fe3330d
lazistar <hsjin1456@gmail.com>
2020-07-12 05:15:45 +0900

    package & __all__
    package 와 __all__ 사용법에 대해서 배웠다.

---
+++ package.py
@@ -0,0 +1,18 @@
+# import travel.thailand
+# # import travel.thailand.ThilandPackage
+# trip_to = travel.thailand.ThilandPackage()
+# trip_to.detail()
+
+# from travel.thailand import ThilandPackage
+# trip_to = ThilandPackage()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+# from random import *
+from travel import *
+# trip_to = vietnam.VietnamPackage()
+trip_to = thailand.ThilandPackage()
+trip_to.detail()
