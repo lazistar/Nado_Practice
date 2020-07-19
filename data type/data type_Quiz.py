418f7f57c873143b6adac6a17f9e3f43bcd737e6
lazistar <hsjin1456@gmail.com>
2020-07-20 05:13:41 +0900

    solution of data type_quiz
    자료형 연습 문제 풀이

---
+++ data type/data type_Quiz.py
@@ -0,0 +1,10 @@
+station = "사당"
+print("{0}행 열차가 들어오고 있습니다.".format(station))
+
+station = "신도림"
+print("{0}행 열차가 들어오고 있습니다.".format(station))
+
+station = "인천공항"
+print("{0}행 열차가 들어오고 있습니다.".format(station))
+
+print(station + "행 열차가 들어오고 있습니다.")
