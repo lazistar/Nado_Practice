6024d134e6cfe2348613016fed193b5ebf803c03
lazistar <hsjin1456@gmail.com>
2020-07-22 02:51:06 +0900

    랜덤함수 및 퀴즈
    랜덤함수 ( from random import * ) 를 라이브러리에서 가져와 활용하는 법을 배웠다.
    또한 관련된 실용예제를 풀어보았다.

---
+++ Operators/Operators_퀴즈.py
@@ -0,0 +1,6 @@
+from random import *
+day = randint(4, 28)
+print("오프라인 스터디 모임 날짜는 매월 {0} 일로 선정되었습니다.".format(day))
+
+date = randint(4, 28)
+print("오프라인 스터디 모임 날짜는 매월 " + str(date) + " 일로 선정되었습니다.")
